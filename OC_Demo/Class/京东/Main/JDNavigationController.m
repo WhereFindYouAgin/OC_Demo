2eecfa2183cd3bcd1a15d84c0b07355b6b52b9ba
work <work@workdeMacBook-Pro.local>
2017-11-16 10:38:04 +0800

    添加了一些东西

--- OC_Demo/Class/京东/Main/JDNavigationController.m
+++ OC_Demo/Class/京东/Main/JDNavigationController.m
@@ -6,7 +6,9 @@
 //  Copyright © 2017年 LUOSU. All rights reserved.
 //
 
-#import "JDNavigationController.h"
+#import "JDNavigationController.h"]
+
+#import "GQGesVCTransition.h"
 
 @interface JDNavigationController ()
 
@@ -20,7 +22,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+     [GQGesVCTransition validateGesBackWithType:GQGesVCTransitionTypePanWithPercentRight withRequestFailToLoopScrollView:YES]; //手势返回;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -28,14 +30,18 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated{
+    if (self.childViewControllers.count >= 1) {
+        //返回按钮自定义
+        viewController.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc]initWithImage:[UIImage imageNamed:@"navigationbar_back"] style:UIBarButtonItemStyleDone target:self action:@selector(backClick)];
+        //影藏BottomBar
+        viewController.hidesBottomBarWhenPushed = YES;
+    }
+    //跳转
+    [super pushViewController:viewController animated:animated];
 }
-*/
+- (void)backClick{
+    [self popViewControllerAnimated:YES];
+}
 
 @end
--- OC_Demo/Class/京东/分类/JDGoodsSetViewController.m
+++ OC_Demo/Class/京东/分类/JDGoodsSetViewController.m
@@ -8,25 +8,42 @@
 
 #import "JDGoodsSetViewController.h"
 #import "JDDataTool.h"
+#import "UIBarButtonItem+Extension.h"
 
 @interface JDGoodsSetViewController ()
-@property (strong , nonatomic)NSArray *setItem;
+@property (strong , nonatomic)NSArray *setItems;
+@property (nonatomic, strong)  UIBarButtonItem *switchViewButton;
 
+/* scrollerVew */
+@property (strong , nonatomic)UICollectionView *collectionView;
+/**
+ 0：列表视图，1：格子视图
+ */
+@property (nonatomic, assign) BOOL isSwitchGrid;
 @end
 
 @implementation JDGoodsSetViewController
 
-- (NSArray*)setItem{
-    if (!_setItem) {
-        _setItem = [JDDataTool recommendItems];
+- (NSArray*)setItems{
+    if (!_setItems) {
+        _setItems = [JDDataTool recommendItems];
     }
-    return _setItem;
+    return _setItems;
 }
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
+    [self setUpNav];
 }
 
-
+- (void)setUpNav{
+    self.switchViewButton = [UIBarButtonItem initWithTarget:self action:@selector(switchViewButtonBarItemBtnClick) image:@"nav_btn_jiugongge" hightImage:@"" selectedImage:@"nav_btn_list"];
+    self.navigationItem.rightBarButtonItem = self.switchViewButton;
+}
+- (void)switchViewButtonBarItemBtnClick
+{
+   UIButton *btn = (UIButton *)self.switchViewButton.customView;
+    btn.selected = !btn.selected;
+}
 
 @end
